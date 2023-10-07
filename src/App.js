5a71cdc15b8fd3dcf4ee47e6e155e28e83ea539f
luisgg121 <luisgg12_1@hotmail.com>
2023-10-07 13:54:35 -0600

    Comit del sábado

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 // import { SectionList, StyleSheet, Text, View } from 'react';
 import { TodoistApi } from "@doist/todoist-api-typescript";
 // import TaskList from './components/TaskList';  
-import './App.css';
+import './app.scss';
 // import axios from 'axios';
 
 const TODOIST_TOKEN = "bf64581b00fb7777bb6865894d61fef991d44c3d"
@@ -17,7 +17,7 @@ function App() {
         api.getProjects()
           .then((projects) => setProjects(projects))
           .catch(() => setProjects([]))
-        // Obtiene la lista de tareas
+        // Obtiene la lista de tareas 
         api.getTasks()
           .then((response) => setTasks(response))
           .catch(() => setTasks([]))
@@ -30,28 +30,81 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <h2>Listado de Cosas por Hacer: </h2>
+    <div class="container">
+      <h2>Listado de Cosas por Hacer- </h2>
       <div>
         {projects.map(project => (
-          <div key={project.id}>
-            <h2>{project.name}</h2>
-            <div>
-              {tasks.filter((f) => f.projectId == project.id).map(task => (
-                <div key={task.id}>
-                  <p>{task.content}</p>
-                </div>
-              ))
-              }
-            </div>
+          <div>
+            <table class="table table-striped table-bordered">
+              <thead>
+                <tr class="table-secondary">
+                  <th data-field="id" data-width="10" data-width-unit="%">{project.id}</th>
+                  <th data-field="name" data-width="50" data-width-unit="%">{project.name}</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* <tr>
+                <td>{project.id}</td>
+                <td>{project.name}</td>
+              </tr> */}
+                {
+                  tasks.filter((f) => f.projectId === project.id).map(task => (
+
+                    <tr>
+                      <td>{task.id}</td>
+                      <td>{task.content}</td>
+                    </tr>
+
+                  ))
+                }
+              </tbody>
+            </table>
+            <p></p>
           </div>
         ))
         }
 
       </div>
     </div>
-
-  );
+  )
 }
+// return (
+//   <div>
+//     <h2>Listado de Cosas por Hacer: </h2>
+//     <div>
+//       <table class="table table-hover">
+//         <thead>
+//           <tr>
+//             <th>ID</th>
+//             <th>DESCRIPCIÓN</th>
+//           </tr>
+//         </thead>
+//         {projects.map(project => (
+//           <tbody>
+//             <tr>
+//               <td>{project.id}</td>
+//               <td>{project.name}</td>
+//             </tr>
+//               {
+//                 tasks.filter((f) => f.projectId === project.id).map(task => (
+
+//                   <tr>
+//                     <td>{task.id}</td>
+//                     <td>{task.content}</td>
+//                   </tr>
+
+//                 ))
+//               }
+//            </tbody>
+//         ))
+//         }
+//        </table>
+//     </div>
+//   </div>
+// )
+
+
+
+
 
 export default App;
