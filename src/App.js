3e89b36a8dded34d3a8a15d54f9f10ca3d7491ab
luisgg121 <luisgg12_1@hotmail.com>
2023-10-07 20:58:29 -0600

    Commit del sábado por la noche

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
-// import { SectionList, StyleSheet, Text, View } from 'react';
+import { SectionList, StyleSheet, Text, View } from 'react';
 import { TodoistApi } from "@doist/todoist-api-typescript";
-// import TaskList from './components/TaskList';  
 import './app.scss';
+import Dropdown from '../node_modules/react-bootstrap/Dropdown';
+
 // import axios from 'axios';
+// import TaskList from './components/TaskList';  
 
 const TODOIST_TOKEN = "bf64581b00fb7777bb6865894d61fef991d44c3d"
 const api = new TodoistApi(TODOIST_TOKEN)
@@ -30,8 +32,49 @@ function App() {
   }, []);
 
   return (
-    <div class="container">
-      <h2>Listado de Cosas por Hacer- </h2>
+    <div class="container-sm">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Menú</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://todoist.com/app/today" target="_blank" >todoist.com</a>
+              </li> 
+
+              <li class="nav-item">
+                <Dropdown>
+                  <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Proyecto
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Todos</Dropdown.Item>
+                    {projects.map(project => (
+                      <Dropdown.Item href="#/action-1">{project.name}</Dropdown.Item>
+                    ))
+                    }
+                  </Dropdown.Menu>
+                </Dropdown>
+              </li>
+            </ul>
+          </div>
+
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            </input>
+            <button class="btn btn-outline-success" type="submit">Búsqueda</button>
+          </form>
+        </div>
+      </nav >
+
+      < h2 > Listado de Cosas por Hacer =+ </h2 >
+
       <div>
         {projects.map(project => (
           <div>
@@ -43,10 +86,6 @@ function App() {
                 </tr>
               </thead>
               <tbody>
-                {/* <tr>
-                <td>{project.id}</td>
-                <td>{project.name}</td>
-              </tr> */}
                 {
                   tasks.filter((f) => f.projectId === project.id).map(task => (
 
@@ -65,45 +104,9 @@ function App() {
         }
 
       </div>
-    </div>
+    </div >
   )
 }
-// return (
-//   <div>
-//     <h2>Listado de Cosas por Hacer: </h2>
-//     <div>
-//       <table class="table table-hover">
-//         <thead>
-//           <tr>
-//             <th>ID</th>
-//             <th>DESCRIPCIÓN</th>
-//           </tr>
-//         </thead>
-//         {projects.map(project => (
-//           <tbody>
-//             <tr>
-//               <td>{project.id}</td>
-//               <td>{project.name}</td>
-//             </tr>
-//               {
-//                 tasks.filter((f) => f.projectId === project.id).map(task => (
-
-//                   <tr>
-//                     <td>{task.id}</td>
-//                     <td>{task.content}</td>
-//                   </tr>
-
-//                 ))
-//               }
-//            </tbody>
-//         ))
-//         }
-//        </table>
-//     </div>
-//   </div>
-// )
-
-
 
 
 
